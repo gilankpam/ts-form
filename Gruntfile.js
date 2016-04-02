907aad3787f8283e1e3a7ef88ed3575925fe17ad
Olivier Andriessen <olivierandriessen@gmail.com>
2016-04-02 15:34:37 +0200

    add templates

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        html2js: {
+            options: {
+                singleModule: true,
+                existingModule: true,
+                module: 'ts-form-templates',
+                rename: function(moduleName) {
+                    return moduleName.replace('../templates/', 'templates/');
+                },
+                htmlmin: {
+                    collapseWhitespace: true,
+                    conservativeCollapse: true,
+                    collapseBooleanAttributes: true,
+                    removeCommentsFromCDATA: true,
+                    removeOptionalTags: true
+                }
+            },
+            build: {
+                src: ['templates/**/*.html'],
+                dest: './lib/ts-form.tpls.js'
+            }
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-html2js');
+};
